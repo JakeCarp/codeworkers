5fba5a4e9bf65f92c6f9447ed9b2b66abb40585f
jacob carpenter <jakecarpenter407@gmail.com>
2021-11-12 11:55:39 -0700

    components templated

---
+++ src/components/ProfileSearchResult.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="profile-search-results"></div>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+</style>
