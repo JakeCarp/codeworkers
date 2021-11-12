2c79ffb5ff2ff2e4614f5a3f526725da36c9be72
jacob carpenter <jakecarpenter407@gmail.com>
2021-11-12 13:55:34 -0700

    Profile Page Drawing

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,19 +1,29 @@
 <template>
   <div class="about text-center">
-    <h1>Welcome {{ profile.name }}</h1>
-    <img class="rounded" :src="profile.picture" alt="" />
-    <p>{{ profile.email }}</p>
+    <h1>Welcome {{ account.name }}</h1>
+    <img class="rounded" :src="account.picture" alt="" />
+    <p>{{ account.email }}</p>
   </div>
 </template>
 
 <script>
-import { computed } from "vue";
+import { computed, reactive, watchEffect } from "vue";
 import { AppState } from "../AppState";
+import { useRoute } from "vue-router";
+import { profileService } from "../services/ProfileService";
 export default {
   name: "Account",
   setup() {
+    const route = useRoute;
+    watchEffect(async () => {
+      if (route.name == "Profile") {
+        await profileService.getProfilebyId(route.params.id);
+        await profileService.getPostsByProfile(route.params.id);
+      }
+    });
     return {
-      account: computed(() => AppState.profile),
+      account: computed(() => AppState.account),
+      profile: computed(() => AppState.profile),
     };
   },
 };
--- src/services/ProfileService.js
+++ src/services/ProfileService.js
@@ -1,4 +1,25 @@
+import { AppState } from "../AppState"
+import { logger } from "../utils/Logger"
+import { api } from "./AxiosService"
+
 class ProfileService {
 
+    async getAllProfiles(query) {
+        const res = await api.get('api/profiles/' + query)
+        logger.log(res.data)
+        AppState.profileList = res.data
+    }
+
+    async getProfilebyId(id) {
+        const res = await api.get('api/profiles/' + id)
+        logger.log(res.data)
+        AppState.profile = res.data
+    }
+    
+    async getPostsByProfile(id) {
+        const res = await api.get('api/profiles/' + id + '/posts')
+        logger.log(res.data)
+        AppState.posts =res.data
+    }
 }
 export const profileService = new ProfileService()
