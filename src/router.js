dc551bd728ce53144e76cd09dbdc5c16672114c0
jacob carpenter <jakecarpenter407@gmail.com>
2021-11-12 11:51:48 -0700

    initialization

--- src/router.js
+++ src/router.js
@@ -17,9 +17,9 @@ const routes = [
     component: loadPage('AboutPage')
   },
   {
-    path: '/account',
-    name: 'Account',
-    component: loadPage('AccountPage'),
+    path: '/profile/:id',
+    name: 'Profile',
+    component: loadPage('ProfilePage'),
     beforeEnter: authGuard
   }
 ]
